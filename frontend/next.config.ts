ccff2360652c532c750c8c38afbaa821cd5fc671
graememooney <graememooney1@gmail.com>
2026-02-06 13:42:26 +0000

    Hardcode backend URL

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,11 +4,8 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        // When running locally, go to localhost. 
-        // When on Vercel, we will use an Environment Variable.
-        destination: process.env.NEXT_PUBLIC_API_URL 
-          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*` 
-          : 'http://127.0.0.1:8003/:path*', 
+        // We are forcing it to use your Live Render Backend
+        destination: 'https://mercosur-backend.onrender.com/:path*',
       },
     ]
   },
